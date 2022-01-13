ab46f07ac347b4f01431c11ebc63b8f43da9be7e
prashant mishra <prashantmishra1995@gmail.com>
2022-01-13 08:44:11 +0530

    day3

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,13 +3,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Catalog App'),
+      ),
       body: Center(
         child: Text('Hi its me'),
       ),
+      drawer: Drawer(),
     );
   }
 }
